c5af0b6f9f448f875088c7b1239bad4320dcd072
박세은 <seeunbag30@gmail.com>
2025-07-09 20:04:10 +0900

    feat: lottie 사용 컴포넌트 추가 및 적용

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,8 +1,21 @@
+import LottieComponent from '@/components/common/LottieComponent'
+import Star from '@/assets/lottie/Star.json'
+
 const MainPage = () => {
   return (
-    <div>
+    <main className='bg-bg-darkgray h-screen max-h-screen w-full'>
+      <LottieComponent
+        animationData={Star}
+        speed={1}
+        isPaused={false}
+        isStopped={false}
+        loop={true}
+        init={0}
+        end={100}
+        className='absolute z-0 h-full w-full opacity-50'
+      />
       <h1 className='text-xl font-thin'>MainPage 당신의 비전을 현실로 그리는 페이지</h1>
-    </div>
+    </main>
   )
 }
 
