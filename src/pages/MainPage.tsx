0445cb5e19f80f49b514a1153c47234c14b9965c
박세은 <seeunbag30@gmail.com>
2025-07-09 20:40:07 +0900

    style: mainpage 첫 랜딩 상태 css 구현

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,7 +3,22 @@ import Star from '@/assets/lottie/Star.json'
 
 const MainPage = () => {
   return (
-    <main className='bg-bg-darkgray h-screen max-h-screen w-full'>
+    <main className='bg-bg-darkgray relative h-screen max-h-screen w-full'>
+      <div className='flex flex-col items-center justify-center gap-37 pt-30'>
+        <header className='z-20 flex flex-col items-center justify-center gap-6'>
+          <h1 className='flex items-center gap-20'>
+            <span className='text-point-darkpurple text-8xl font-extrabold'>D A W N</span>
+            <span className='text-point-darkpurple text-8xl font-extrabold'>S T R E E T</span>
+          </h1>
+          <h2 className='text-text-gray text-4xl font-extralight'>
+            당신의 비전을 현실로 그려내는 공간
+          </h2>
+        </header>
+        <button className='border-text-gray text-text-gray hover:bg-point-darkpurple/30 z-20 h-24 w-sm cursor-pointer rounded-full border text-4xl font-medium transition-all duration-200 hover:text-white'>
+          우리의 여정
+        </button>
+      </div>
+
       <LottieComponent
         animationData={Star}
         speed={1}
@@ -12,9 +27,15 @@ const MainPage = () => {
         loop={true}
         init={0}
         end={100}
-        className='absolute z-0 h-full w-full opacity-50'
+        className='absolute top-0 z-0 h-full w-full opacity-50'
       />
-      <h1 className='text-xl font-thin'>MainPage 당신의 비전을 현실로 그리는 페이지</h1>
+
+      <img
+        src='./src/assets/png/city.png'
+        alt='city image'
+        className='absolute bottom-12.5 z-10 w-full brightness-20'
+      />
+      <div className='bg-bg-black absolute bottom-0 z-10 h-12.5 w-full' />
     </main>
   )
 }
