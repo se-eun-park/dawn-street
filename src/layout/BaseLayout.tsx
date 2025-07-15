1dc0160fbe38313a5993cc2bb83169544df03278
SeEun Park <seeunbag30@gmail.com>
2025-07-15 13:17:04 +0900

    feat: lenis 적용

--- src/layout/BaseLayout.tsx
+++ src/layout/BaseLayout.tsx
@@ -1,10 +1,26 @@
-import { MainPage } from '@/pages'
+import gsap from 'gsap'
+import { ReactLenis } from 'lenis/react'
+import { useEffect, useRef } from 'react'
+import { MainPage, ProjectPage } from '@/pages'
 
 const BaseLayout = () => {
+  const lenisRef = useRef<any>(null)
+
+  useEffect(() => {
+    function update(time: number) {
+      lenisRef.current?.lenis?.raf(time * 1000)
+    }
+
+    gsap.ticker.add(update)
+
+    return () => gsap.ticker.remove(update)
+  }, [])
+
   return (
     <main>
+      <ReactLenis root options={{ autoRaf: false }} ref={lenisRef} />
       <MainPage />
-      {/* <ProjectPage /> */}
+      <ProjectPage />
     </main>
   )
 }
